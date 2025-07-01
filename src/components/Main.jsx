4f1a70717ab64fd478bdc7652232c7cf8d1df97a
jmih_pajiloy <svanyokshib@gmail.com>
2025-07-02 02:30:17 +0300

    optimised

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Event } from "./Event";
+import {Event} from "./Event";
 
 const TABS = {
   "all": {
@@ -2336,96 +2336,8 @@ export function Main() {
     }
   };
 
-  return <main className="main">
-    <section className="section main__general">
-      <h2 className="section__title section__title-header section__main-title">Главное</h2>
-      <div className="hero-dashboard">
-        <div className="hero-dashboard__primary">
-          <h3 className="hero-dashboard__title">Привет, Геннадий!</h3>
-          <p className="hero-dashboard__subtitle">Двери и окна закрыты, сигнализация включена.</p>
-          <ul className="hero-dashboard__info">
-            <li className="hero-dashboard__item">
-              <div className="hero-dashboard__item-title">Дома</div>
-              <div className="hero-dashboard__item-details">
-                +23
-                <span className="a11y-hidden">°</span>
-              </div>
-            </li>
-            <li className="hero-dashboard__item">
-              <div className="hero-dashboard__item-title">За окном</div>
-              <div className="hero-dashboard__item-details">
-                +19
-                <span className="a11y-hidden">°</span>
+  return <>
 
-                <div
-                    className="hero-dashboard__icon hero-dashboard__icon_rain"
-                    role="img"
-                    aria-label="Дождь"
-                ></div>
-              </div>
-            </li>
-          </ul>
-        </div>
-        <ul className="hero-dashboard__schedule">
-          <Event
-              icon="temp"
-              iconLabel="Температура"
-              title="Philips Cooler"
-              subtitle="Начнет охлаждать в 16:30"
-          />
-          <Event
-              icon="light"
-              iconLabel="Освещение"
-              title="Xiaomi Yeelight LED Smart Bulb"
-              subtitle="Включится в 17:00"
-          />
-          <Event
-              icon="light"
-              iconLabel="Освещение"
-              title="Xiaomi Yeelight LED Smart Bulb"
-              subtitle="Включится в 17:00"
-          />
-        </ul>
-      </div>
-    </section>
-
-    <section className="section main__scripts">
-      <h2 className="section__title section__title-header">Избранные сценарии</h2>
-
-      <ul className="event-grid">
-        <Event
-            slim
-            icon="light2"
-            iconLabel="Освещение"
-            title="Выключить весь свет в доме и во дворе"
-        />
-        <Event
-            slim
-            icon="schedule"
-            iconLabel="Расписание"
-            title="Я ухожу"
-        />
-        <Event
-            slim
-            icon="light2"
-            iconLabel="Освещение"
-            title="Включить свет в коридоре"
-        />
-        <Event
-            slim
-            icon="temp2"
-            iconLabel="Температура"
-            title="Набрать горячую ванну"
-            subtitle="Начнётся в 18:00"
-        />
-        <Event
-            slim
-            icon="temp2"
-            iconLabel="Температура"
-            title="Сделать пол тёплым во всей квартире"
-        />
-      </ul>
-    </section>
 
     <section className="section main__devices">
       <div className="section__title">
@@ -2480,5 +2392,5 @@ export function Main() {
         }
       </div>
     </section>
-  </main>;
+  </>;
 }
