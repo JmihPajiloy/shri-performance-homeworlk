1eaf7af5d10424b9311df9e31a38467107d2fa97
jmih_pajiloy <svanyokshib@gmail.com>
2025-07-02 02:05:11 +0300

    migrated to vite

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {Header} from "./components/Header";
+import {Main} from "./components/Main";
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(
+    <>
+      <Header/>
+      <Main/>
+    </>
+);
