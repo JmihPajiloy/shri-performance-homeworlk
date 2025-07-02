2501000e09854b77d3f0915d770d8e7ff78a35c6
jmih_pajiloy <svanyokshib@gmail.com>
2025-07-02 16:01:50 +0300

    rewrote `Header` component into pure js

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {Header} from "./components/Header";
+
 import {Main} from "./components/Main";
 
 const mainRoot = ReactDOM.createRoot(document.getElementById('main'));
 
-const headerRoot = ReactDOM.createRoot(document.getElementById("header"));
-headerRoot.render(<Header/>)
 mainRoot.render(<Main/>);
